79b2e7c2f0e870d9e33dcb4403478fe79c30a12d
anyhelp000 <shaikyusuf1998@gmail.com>
2026-05-01 09:07:44 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import com.daily.practise.Practise;
+
+class Main{
+    public static void main(String[] args) {
+        Practise p = new Practise();
+        String message = p.message();
+        System.out.println(message);
+    }
+}
---
+++ src/com/daily/practise/Practise.java
@@ -0,0 +1,7 @@
+package com.daily.practise;
+
+public class Practise {
+    public String message(){
+        return "Hello";
+    }
+}
