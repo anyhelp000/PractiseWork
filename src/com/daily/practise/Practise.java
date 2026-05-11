14749e49fc2324a1afc1e1575522fb6e75c1f51e
anyhelp000 <shaikyusuf1998@gmail.com>
2026-05-12 00:08:00 +0530

    added in feature

--- src/com/daily/practise/Practise.java
+++ src/com/daily/practise/Practise.java
@@ -2,7 +2,7 @@ package com.daily.practise;
 
 public class Practise {
     public String message(){
-        System.out.println("Hi");
+        System.out.println("added in feature");
         return "changed in master";
     }
 }
