4c99a11d28abbbdb2299f106e3556246c0e2b23a
anyhelp000 <shaikyusuf1998@gmail.com>
2026-05-11 23:50:13 +0530

    changed in master 1

--- src/com/daily/practise/Practise.java
+++ src/com/daily/practise/Practise.java
@@ -3,6 +3,6 @@ package com.daily.practise;
 public class Practise {
     public String message(){
         System.out.println("Hi");
-        return "changed in master change";
+        return "changed in master";
     }
 }
