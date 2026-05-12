573ebfcfcbdca399f79cdbd36c6565ff4e477299
anyhelp000 <shaikyusuf1998@gmail.com>
2026-05-12 07:26:12 +0530

    added in master_master

--- src/com/daily/practise/Practise.java
+++ src/com/daily/practise/Practise.java
@@ -2,7 +2,7 @@ package com.daily.practise;
 
 public class Practise {
     public String message(){
-        System.out.println("Hi");
+        System.out.println("changed in master_master");
         return "changed in master";
     }
 }
