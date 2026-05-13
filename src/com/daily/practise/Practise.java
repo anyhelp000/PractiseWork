89adf1efb494ddbe2416eee3bfb49113bd244e71
anyhelp000 <shaikyusuf1998@gmail.com>
2026-05-13 22:38:26 +0530

    master changes

--- src/com/daily/practise/Practise.java
+++ src/com/daily/practise/Practise.java
@@ -2,7 +2,8 @@ package com.daily.practise;
 
 public class Practise {
     public String message(){
-        System.out.println("change in master");
+        System.out.println("change in master bro");
+        //will see
         return " ";
     }
 }
